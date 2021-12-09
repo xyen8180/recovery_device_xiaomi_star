cd7bf410d5315dd1379b7aae98c32742a336b272
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2021-12-08 22:16:22 -0800

    added drone ci

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp -b twrp-11"
-
-
+MANIFEST="https://gitlab.com/OrangeFox/sync.git"
 DEVICE=star
-DT_LINK="https://github.com/xyen8180/test-tree -b test"
+DT_LINK="https://github.com/xyen8180/recovery_device_xiaomi_star"
 DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
@@ -15,30 +13,22 @@ apt update --fix-missing
 apt install openssh-server -y
 mkdir ~/twrp && cd ~/twrp
 
-echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST
-repo sync
-repo sync
-git clone --depth=1 $DT_LINK $DT_PATH
+echo " ===+++ Sync OrangeFox +++==="
+git clone $MANIFEST ~/FOX && cd ~/FOX
+./orangefox_sync.sh --branch 11.0 --path ~/fox_11.0
+cd ~/fox_11.0
+git clone $DT_LINK $DT_PATH
 
-echo " ===+++ Building Recovery +++==="
-
+echo " ====+++ Building OrangeFox +++==="
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch twrp_${DEVICE}-eng && mka bootimage
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export LC_ALL="C"
+lunch twrp_${DEVICE}-eng && mka recoveryimage
 
-cd $OUT/recovery/root
-./ldcheck -p system/lib64:vendor/lib64 -d system/bin/qseecomd
-cd -
-
-# Upload zips & boot.img (U can improvise lateron adding telegram support etc etc)
-echo " ===+++ Uploading Recovery +++==="
-version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+# Upload zips & recovery.img
+#echo " ===+++ Uploading Recovery +++===
 cd out/target/product/$DEVICE
-mv boot.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
-#curl -T $OUTFILE https://oshi.at
-curl -sL $OUTFILE https://git.io/file-transfer | sh
+curl -sL https://git.io/file-transfer | sh
 ./transfer wet *.zip
